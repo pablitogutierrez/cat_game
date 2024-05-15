4dbbebbab5f37b40fe2ef3ad5a7fe90ee36697ef
PabloGutierrez1 <genesislol660@gmail.com>
2024-05-15 15:25:20 -0300

    15/5/2024

--- functions.js
+++ functions.js
@@ -37,9 +37,37 @@ let btn_c = document.getElementById('btn-c');
 let btn_s = document.getElementById('btn-s');
 let title = document.getElementById('title');
 
+let menu_c = document.getElementById('menu-c');
+let btn_skip_c = document.getElementById('btn-skip-c');
+
+let menu_s = document.getElementById('menu-s');
+let btn_skip_s = document.getElementById('btn-skip-s');
+
 btn_p.addEventListener('click', () => { 
     btn_p.classList.add('hide-down');
     btn_c.classList.add('hide-up');
     btn_s.classList.add('hide-up');
     title.classList.add('hide-up');
+})
+
+btn_c.addEventListener('click', () => {
+    btn_s.classList.toggle('hide-up');
+    menu_c.classList.toggle('show-menu');
+})
+
+btn_skip_c.addEventListener('click', () => {
+    btn_c.classList.remove('hide-up');
+    btn_s.classList.remove('hide-up');
+    menu_c.classList.remove('show-menu');
+})
+
+btn_s.addEventListener('click', () => {
+    btn_c.classList.toggle('hide-up');
+    menu_s.classList.toggle('show-menu');
+})
+
+btn_skip_s.addEventListener('click', () => {
+    btn_c.classList.remove('hide-up');
+    btn_s.classList.remove('hide-up');
+    menu_s.classList.remove('show-menu');
 })
